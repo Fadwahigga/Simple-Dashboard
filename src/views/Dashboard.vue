70317dfd8ef9e1cb9780f534db4121ff81a0d495
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-18 21:05:44 +0300

    minu

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="w-screen h-screen flex">
         <!-- Sidebar -->
-        <div class="w-52 h-screen bg-gray-200"> <div class="h-14 bg-gray-300">Header sidebar</div></div>
+        <div class="w-52 h-screen bg-gray-200">
+            <div class="h-14 bg-gray-700">Header sidebar</div>
+            <div class="h-14 bg-gray-700">minu</div>
+        </div>
         <!-- main -->
         <div class="w-full h-full bg-gray-400"> main</div>
     </div>
