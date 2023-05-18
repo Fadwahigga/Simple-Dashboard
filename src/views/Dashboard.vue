f06fa27f8a2f5a51215b2d004702d4dd33cb4df0
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-18 21:33:10 +0300

    Home

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-screen h-screen flex text-white text-center">
+    <div class="w-screen h-screen flex text-white ">
         <!-- Sidebar -->
         <div class="w-52 h-screen bg-gray-900">
-            <div class="h-14 bg-gray-900"><h1 class="font-bold text-xl ">Admin Dashboard</h1></div>
-            <div class="h-14 bg-gray-700">minu</div>
+            <div class="h-14 bg-gray-900"><h1 class="font-bold text-xl mx-1">Admin Dashboard</h1></div>
+            <div class="h-full bg-gray-700 flex flex-col"><div>Home</div></div>
         </div>
         <!-- main -->
         <div class="w-full h-full bg-gray-500">  <div class="h-14 bg-gray-900">Header sidebar</div>
