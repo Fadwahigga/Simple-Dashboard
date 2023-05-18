553a9fc704bbdca3dd84f1c1002fd652042d0436
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-18 21:04:58 +0300

    Header sidebar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-screen h-screen flex">
         <!-- Sidebar -->
-        <div class="w-52 h-screen bg-gray-200"> Sidebar</div>
+        <div class="w-52 h-screen bg-gray-200"> <div class="h-14 bg-gray-300">Header sidebar</div></div>
         <!-- main -->
         <div class="w-full h-full bg-gray-400"> main</div>
     </div>
