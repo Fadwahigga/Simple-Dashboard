19e94cd1dba589ea5ac49e748fae45280c75cb46
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-18 21:20:05 +0300

    Admin Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,7 @@
     <div class="w-screen h-screen flex text-white text-center">
         <!-- Sidebar -->
         <div class="w-52 h-screen bg-gray-200">
-            <div class="h-14 bg-gray-700">Header sidebar</div>
+            <div class="h-14 bg-gray-700"><h3 class="font-bold text-xl">Admin Dashboard</h3></div>
             <div class="h-14 bg-gray-900">minu</div>
         </div>
         <!-- main -->
