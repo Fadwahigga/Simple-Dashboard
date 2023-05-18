97d7f8438d6f7d9539faf10d31daca2f65907155
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-18 21:41:59 +0300

    profile

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -16,6 +16,16 @@
                     </svg>
                     Home
                 </router-link>
+                <router-link to="/profile"
+                    class="inline-flex relative items-center p-2 w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
+                    <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor"
+                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    Profile
+                </router-link>
                 <router-link to="/home"
                     class="inline-flex relative items-center p-2 w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800  transition duration-400 ease-in-out">
                     <svg aria-hidden="true" class="mr-2 w-[25px] h-[25px] fill-current" fill="currentColor"
@@ -26,6 +36,7 @@
                     </svg>
                     Profile
                 </router-link>
+                
             </div>
         </div>
         <!-- main -->
